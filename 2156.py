f8cbb975c41a7e07af120576d12af373c0d23d03
minoring <minho1350@gmail.com>
2019-06-02 00:04:45 +0900

    19-06-01

--- 2156.py
+++ 2156.py
@@ -1,16 +1,5 @@
-n = int(input())
-c = [int(input()) for _ in range(n)]
-c = [0] + c
-
-z = [0] * (n + 1)
-o = [0] * (n + 1)
-t = [0] * (n + 1)
-
-o[1] = c[1]
-
-for i in range(2, n + 1):
-    z[i] = max(o[i - 1], t[i - 1])
-    o[i] = max(z[i - 1], z[i - 2]) + c[i]
-    t[i] = o[i - 1] + c[i]
-
-print(max(z[n], max(o[n], t[n])))
+z = o = t = 0
+for _ in range(int(input())):
+    n = int(input())
+    z, o, t = max(z, o, t), z + n, o + n
+print(max(z, o, t))
