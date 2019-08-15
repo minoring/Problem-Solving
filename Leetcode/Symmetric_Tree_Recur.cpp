b798ffe301ed677cb5fd4662f51e297660d08990
minoring <minho1350@gmail.com>
2019-08-15 13:11:54 -0700

    19-08-15

---
+++ Leetcode/Symmetric_Tree_Recur.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+   public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        return isSymmetricHelp(root->left, root->right);
+    }
+
+    bool isSymmetricHelp(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr) {
+            return true;
+        }
+        if (left == nullptr || right == nullptr) {
+            return false;
+        }
+        return ((left->val == right->val) &&
+                isSymmetricHelp(left->left, right->right) &&
+                isSymmetricHelp(left->right, right->left));
+    }
+};
