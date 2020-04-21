efebe4b52c5eff722d82c7b21e6722c4674e1d27
minoring <minho1350@gmail.com>
2020-04-20 22:39:45 -0700

    20-04-20

---
+++ BJ-Algo/14681.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+  int x;
+  int y;
+  std::cin >> x >> y;
+
+  if (x > 0) {
+    if (y > 0)
+      std::cout << 1;
+    else
+      std::cout << 4;
+  } else {
+    if (y > 0)
+      std::cout << 2;
+    else
+      std::cout << 3;
+  }
+}
