f7487415e30447c0dc4077503940a19840fa181a
minoring <minho1350@gmail.com>
2020-04-23 21:49:03 -0700

    20-04-23

--- Leetcode/MinStack.cpp
+++ Leetcode/MinStack.cpp
@@ -1,30 +1,41 @@
-/*
-https://leetcode.com/problems/min-stack/
-Approach: two stack
-Create one normal stack and min stack.
-insert into min stack if input is smallest value.
-pop from min stack if it is the value on the top of normal stack
-
-Time Complexity: O(1)
-Space Complexity: O(n)
-*/
-#include <stack>
-
-using namespace std;
-
 class MinStack {
-private:
-    stack<int> s1;
-    stack<int> s2;
 public:
+    /** initialize your data structure here. */
+    stack<pair<int, int>> s;
+
+    MinStack() {
+    }
+
     void push(int x) {
-        s1.push(x);
-        if (s2.empty() || x <= getMin()) s2.push(x);
+        if (s.empty()) {
+            s.push({x, x});
+        } else {
+            s.push({x, min(x, s.top().second)});
+        }
+
+
     }
+
     void pop() {
-        if (s2.top() == s1.top()) s2.pop();
-        s1.pop();
+        if (!s.empty()) {
+            s.pop();
+        }
     }
-    int top() { return s1.top(); }
-    int getMin() { return s2.top(); }
+
+    int top() {
+        return s.top().first;
+    }
+
+    int getMin() {
+        return s.top().second;
+    }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
