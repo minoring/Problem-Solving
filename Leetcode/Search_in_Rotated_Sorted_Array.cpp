9705da29d142f32ea99182d8c60a77a6bd8cfe30
minoring <minho1350@gmail.com>
2020-04-28 21:57:15 -0700

    20-04-28

--- Leetcode/Search_in_Rotated_Sorted_Array.cpp
+++ Leetcode/Search_in_Rotated_Sorted_Array.cpp
@@ -1,3 +1,6 @@
+/*
+Binary tree is about true false
+*/
 #include <vector>
 
 using namespace std;
@@ -7,7 +10,7 @@ public:
     int search(vector<int>& nums, int target) {
         int lo = 0;
         int hi = nums.size() - 1;
-        
+
         while (lo < hi) {
             int mid = (lo + hi) / 2;
             if (nums[mid] > nums[hi]) {
@@ -21,14 +24,14 @@ public:
         return max(binary_search(nums, target, 0, rot - 1),
                    binary_search(nums, target, rot, nums.size() - 1));
     }
-    
+
     int binary_search(vector<int>& nums, int target, int lo, int hi) {
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            
+
             if (nums[mid] < target) {
                 lo = mid + 1;
             } else {
