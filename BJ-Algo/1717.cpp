5564dc2833d19d58ccfee117a8bc912aade2f9ca
minoring <minho1350@gmail.com>
2020-05-08 16:01:23 -0700

    20-05-08

--- BJ-Algo/1717.cpp
+++ BJ-Algo/1717.cpp
@@ -1,42 +1,47 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-int parent[1000001];
-
-int find(int x) {
-    if (parent[x] == x) {
-        return x;
-    }
-    parent[x] = find(parent[x]);
-    return parent[x];
+int Find(int a, unordered_map<int, int> &m_s)
+{
+  if (m_s[a] == a) {
+    return a;
+  }
+  return m_s[a] = Find(m_s[a], m_s);
 }
 
-void uni(int a, int b) {
-    int p_a = find(a);
-    int p_b = find(b);
-    parent[p_a] = p_b;
-}
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+  int n;
+  int m;
+  unordered_map<int, int> m_s;
 
-    int n;
-    int m;
-    cin >> n >> m;
-    for (int i = 0; i < 1000001; ++i) {
-        parent[i] = i;
+  cin >> n >> m;
+  for (int i = 0; i <= n; ++i) {
+    m_s.insert({i, i});
+  }
+
+  for (int i = 0; i < m; ++i) {
+    int c, a, b;
+    cin >> c >> a >> b;
+    if (c == 1) {
+      int u = Find(a, m_s);
+      int v = Find(b, m_s);
+      if (u == v) {
+        m_s[u] = v;
+        cout << "YES" << '\n';
+      } else {
+        cout << "NO" << '\n';
+      }
+    } else {
+      int u = Find(a, m_s);
+      int v = Find(b, m_s);
+      m_s[u] = v;
     }
-
-    for (int i = 0; i < m; ++i) {
-        int c, a, b;
-        cin >> c >> a >> b;
-        if (c == 0) {
-            uni(a, b);
-        } else {
-            cout << (find(a) == find(b) ? "YES" : "NO") << '\n';
-        }
-    }
+  }
 }
