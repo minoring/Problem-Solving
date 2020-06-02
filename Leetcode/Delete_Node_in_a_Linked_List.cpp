e4033320f1aea30ca8d8171d398c0c522accd66e
minoring <minho1350@gmail.com>
2020-06-02 16:30:33 -0700

    20-06-02

--- Leetcode/Delete_Node_in_a_Linked_List.cpp
+++ Leetcode/Delete_Node_in_a_Linked_List.cpp
@@ -7,9 +7,9 @@
  * };
  */
 class Solution {
- public:
-  void deleteNode(ListNode* node) {
-    ListNode* temp = node->next;
+public:
+  void deleteNode(ListNode *node) {
+    ListNode *temp = node->next;
     node->val = temp->val;
     node->next = temp->next;
     delete temp;
