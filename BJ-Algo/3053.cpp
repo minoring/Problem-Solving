fce55d103c316cb0a5e699bd2414abbe24dc6597
minoring <minho1350@gmail.com>
2020-06-22 18:18:00 +0900

    20-06-22

---
+++ BJ-Algo/3053.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  double R;
+  scanf("%lf", &R);
+  printf("%.6lf\n", M_PI * R * R);
+  printf("%.6lf\n", 2 * R * R);
+}
