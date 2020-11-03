e7836087e18db80d0951f2812c19691c87f6ee03
minoring <minho1350@gmail.com>
2020-11-04 00:18:22 +0900

    20-11-03

--- jm-book/ch7/reverse_quad_tree.cpp
+++ jm-book/ch7/reverse_quad_tree.cpp
@@ -1,61 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-struct QuadTree {
-    char me;
-    QuadTree *q;
-    QuadTree *w;
-    QuadTree *e;
-    QuadTree *r;
-
-    QuadTree(char me)
-        : me(me), q(nullptr), w(nullptr), e(nullptr), r(nullptr) {}
-};
-
-QuadTree *input() {
-    char c;
-    cin >> c;
-    QuadTree *root = new QuadTree(c);
-
-    if (c == 'x') {
-        root->q = input();
-        root->w = input();
-        root->e = input();
-        root->r = input();
+string quad(string::iterator &it) {
+    char root = *it;
+    it++;
+    if (root == 'b' || root == 'w') {
+        return string(1, root);
     }
 
-    return root;
-}
+    string q = quad(it);
+    string w = quad(it);
+    string e = quad(it);
+    string r = quad(it);
 
-void reverse(QuadTree *root) {
-    if (root == nullptr) {
-        return;
-    }
-
-    if (root->me == 'w' || root->me == 'b') {
-        return;
-    }
-
-    reverse(root->q);
-    reverse(root->e);
-    swap(root->q, root->e);
-
-    reverse(root->w);
-    reverse(root->r);
-    swap(root->w, root->r);
-}
-
-void print(const QuadTree *root) {
-    if (root == nullptr) {
-        return;
-    }
-
-    cout << root->me;
-    print(root->q);
-    print(root->w);
-    print(root->e);
-    print(root->r);
+    return string("x") + e + r + q + w;
 }
 
 int main() {
@@ -63,9 +23,9 @@ int main() {
     cin >> c;
 
     while (c--) {
-        QuadTree *root = input();
-        reverse(root);
-        print(root);
-        cout << '\n';
+        string s;
+        cin >> s;
+        string::iterator it = s.begin();
+        cout << quad(it) << '\n';
     }
 }
